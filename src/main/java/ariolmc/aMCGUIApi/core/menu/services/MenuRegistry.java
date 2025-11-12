46b4845e65992290aa06484a7efa3316d914ddcf
Nylers08 <scindeled@gmail.com>
2025-11-12 19:41:46 +0500

    Кривоватый MenuRegistry

--- src/main/java/ariolmc/aMCGUIApi/core/menu/services/MenuRegistry.java
+++ src/main/java/ariolmc/aMCGUIApi/core/menu/services/MenuRegistry.java
@@ -2,19 +2,29 @@ package ariolmc.aMCGUIApi.core.menu.services;
 
 import ariolmc.aMCGUIApi.core.menu.someMenu.Menu;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 public class MenuRegistry {
 
-    private final Map<UUID, Menu> openMenus = new HashMap<>();
+    private final Map<UUID, Menu> playerMenus = new HashMap<>();
+    private final Map<Menu, Set<UUID>> menuViewers = new HashMap<>();
 
     public void register(UUID playerId, Menu menu){
-        openMenus.put(playerId, menu);
+        playerMenus.put(playerId, menu);
+        menuViewers.computeIfAbsent(menu, v -> new HashSet<>()).add(playerId);
+    }
+
+    public void unregister(UUID playerId){
+        Menu menu = playerMenus.get(playerId);
+        playerMenus.remove(playerId);
+        menuViewers.get(menu).remove(playerId);
     }
 
     public Menu getMenu(UUID playerId){
-        return openMenus.get(playerId);
+        return playerMenus.get(playerId);
+    }
+
+    public Set<UUID> getViewers(Menu menu){
+        return menuViewers.get(menu);
     }
 }
