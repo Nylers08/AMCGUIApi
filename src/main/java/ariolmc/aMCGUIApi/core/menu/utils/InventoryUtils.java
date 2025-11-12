5c0f3e983d048df07ee4df2fc4d5b9fd23d2bc31
Nylers08 <scindeled@gmail.com>
2025-11-12 13:21:37 +0500

    Метод для слияния двух инвентарей

--- src/main/java/ariolmc/aMCGUIApi/core/menu/utils/InventoryUtils.java
+++ src/main/java/ariolmc/aMCGUIApi/core/menu/utils/InventoryUtils.java
@@ -2,7 +2,9 @@ package ariolmc.aMCGUIApi.core.menu.utils;
 
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class InventoryUtils {
 
@@ -10,5 +12,22 @@ public class InventoryUtils {
         Inventory newInv = Bukkit.createInventory(null, inv.getSize(), title);
         newInv.setContents(inv.getContents());
         return newInv;
+    }
+
+    public static Inventory merge(Inventory source, Inventory target, Component title){
+        Inventory newInv = copyWithNewTitle(target, title);
+
+        ItemStack[] contents = source.getContents();
+        setContentsWithoutAir(newInv, contents);
+
+        return newInv;
+    }
+
+    private static void setContentsWithoutAir(Inventory inv, ItemStack[] contents){
+        for (int i = 0; i < contents.length; i++) {
+            ItemStack item = contents[i];
+            if(item.getType().equals(Material.AIR)) continue;
+            inv.setItem(i, item);
+        }
     }
 }
