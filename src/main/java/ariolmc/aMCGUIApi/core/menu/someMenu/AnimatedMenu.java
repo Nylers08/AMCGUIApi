941fa86ea034bc86fad3b06e5957ebc33bc5e27f
Nylers08 <scindeled@gmail.com>
2025-11-12 13:46:02 +0500

    Простенькое проигрывание анимации у AnimatedMenu

--- src/main/java/ariolmc/aMCGUIApi/core/menu/someMenu/AnimatedMenu.java
+++ src/main/java/ariolmc/aMCGUIApi/core/menu/someMenu/AnimatedMenu.java
@@ -1,14 +1,25 @@
 package ariolmc.aMCGUIApi.core.menu.someMenu;
 
+import ariolmc.aMCGUIApi.core.menu.utils.InventoryUtils;
 import ariolmc.aMCGUIApi.infrastructure.inventoryFabric.InventoryFabric;
+import net.kyori.adventure.text.Component;
+import org.bukkit.inventory.Inventory;
+
+import java.util.List;
 
 public class AnimatedMenu extends Menu{
 
+    private final List<Inventory> frames;
+    private int currentFrame = 0;
+    private double secondsBetweenFrame = 5;
 
-
-    public AnimatedMenu(InventoryFabric fabric) {
+    public AnimatedMenu(InventoryFabric fabric, List<Inventory> frames) {
         super(fabric);
+        this.frames = frames;
     }
 
-
+    public void nextFrame(){
+        currentFrame = currentFrame<frames.size() ? currentFrame+1 : 0;
+        inventory = InventoryUtils.copyWithNewTitle(frames.get(currentFrame), Component.text("asd"));
+    }
 }
