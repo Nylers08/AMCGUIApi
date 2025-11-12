665b886f0fbf9e8eed22057129aca5e003bd3f0e
Nylers08 <scindeled@gmail.com>
2025-11-12 13:22:04 +0500

    Потихоньку начинаем работать над анимированным меню

---
+++ src/main/java/ariolmc/aMCGUIApi/core/menu/someMenu/AnimatedMenu.java
@@ -0,0 +1,14 @@
+package ariolmc.aMCGUIApi.core.menu.someMenu;
+
+import ariolmc.aMCGUIApi.infrastructure.inventoryFabric.InventoryFabric;
+
+public class AnimatedMenu extends Menu{
+
+
+
+    public AnimatedMenu(InventoryFabric fabric) {
+        super(fabric);
+    }
+
+
+}
