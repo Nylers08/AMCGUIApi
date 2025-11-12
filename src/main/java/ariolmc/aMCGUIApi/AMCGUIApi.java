b5fff519a8d44a2fc56941364bd3bf6b7bd0d31a
Nylers08 <scindeled@gmail.com>
2025-11-12 09:41:19 +0500

    Начало работы над GUIApi

---
+++ src/main/java/ariolmc/aMCGUIApi/AMCGUIApi.java
@@ -0,0 +1,17 @@
+package ariolmc.aMCGUIApi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AMCGUIApi extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
